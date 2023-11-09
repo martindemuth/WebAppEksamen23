7ac7382e9f4aea95e4dc45f76c07fe9c581954bb
Oliver Kimmell <oliverdk@hiof.no>
2023-11-09 14:20:08 +0100

    Opprettet Result component

---
+++ oppgave_1/src/components/Result.tsx
@@ -0,0 +1,14 @@
+import { createTasks } from "@/features/createTasks"
+import { type Task } from "@/types"
+
+const taskList = createTasks
+
+
+export default function Result (/*answers: Map<Task["id"], { attempts: number, type: Task["type"]}>*/) {
+
+    const answers = new Map<Task["id"], { attempts: number, type: Task["type"]}>()
+
+    
+
+
+}
