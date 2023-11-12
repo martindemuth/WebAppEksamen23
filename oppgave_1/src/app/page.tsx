84b9eb43b410cea0b14d36df5f24a677b20e786e
Mathiadr <mathiadr@hiof.no>
2023-11-12 19:29:48 +0100

    Refaktoriserte TaskContext

--- oppgave_1/src/app/page.tsx
+++ oppgave_1/src/app/page.tsx
@@ -7,16 +7,17 @@ import { type Task } from "@/types"
 import Tasks from "@/components/Tasks"
 import TaskText from "@/components/Text"
 import {TaskProvider} from "@/features/TaskContext"
+import { useEffect } from "react"
+import useTasks from "@/hooks/useTasks"
 
 
 export default function Home() {
   const url = `http://localhost:${process.env.NEXT_PUBLIC_PORT ?? 3000}/api/tasks`
-  
 
   return (
     <main>
       <Header />
-      <TaskProvider url={url}>
+      <TaskProvider url={url} count={10}>
         <Tasks>
           <Answer />
         </Tasks>
---
+++ oppgave_1/src/hooks/useTasks.tsx
@@ -0,0 +1,26 @@
+import { Task } from "@/types";
+import { useEffect, useState } from "react";
+
+// Ikke bruk denne
+export default function useTasks(url: string, count: number){
+    const [tasks, setTasks] = useState<Task[]>([])
+
+    useEffect(() => {
+        async function getTasks(count: number) {
+            const response = await fetch(url + `?count=${count}`, {
+                method: "GET"
+            })
+            const result = (await response.json()) as { data: Task[], success: boolean, error: string}
+            if(result.success) setTasks(result.data)
+            else console.error(result.error)
+        }
+        getTasks(count)
+    }, [])
+
+    
+    
+    return {
+        tasks,
+        setTasks
+    }
+}
