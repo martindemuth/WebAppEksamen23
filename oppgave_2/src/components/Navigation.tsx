05039c51e879896a7343b042b3b1c8b8c14fc291
olikim <90384162+olikim@users.noreply.github.com>
2023-11-14 12:56:23 +0100

    Added Navigation component

---
+++ oppgave_2/src/components/Navigation.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+
+export default function Navigation() {
+    const pathname = usePathname()
+
+    const routes = [
+        {
+            label: "Home",
+            href: "/"
+        },
+        {
+            label: "Performers",
+            href: "/performers"
+        },
+        {
+            label: "Goals",
+            href: "/goals"
+        }
+    ]
+
+    const checkActivePath = (path: string) => {
+        return path === pathname
+      }
+
+
+      return(
+        <nav className="bg-blue-500 py-2 px-4 text-white flex justify-between items-center">
+            {routes.map((route)=>(
+                <Link
+                key={route.href} 
+                href={route.href} 
+                className={`${checkActivePath(route.href) ? "underline text-xl font-bold" : ""} hover:text-yellow-300`}>
+                    {route.label}
+                </Link>
+            ))}   
+        </nav>
+    )
+}
