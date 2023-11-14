388cf3dd60779368fa50c23cdbb20c19e8a45117
Martin Demuth <martindemuth@wican.no>
2023-11-14 12:53:47 +0100

    Types for oppgave 2

---
+++ oppgave_2/src/types/index.ts
@@ -0,0 +1,21 @@
+export type Performer = {
+    id: string
+    gender: string
+    sport: string
+}
+export type Competition = {
+    name: string
+    date: Date
+    loaction: string
+    goal: string
+    priority: string
+    comment: string
+
+}
+export type Goal = {
+    name: string
+    date: Date
+    goal: number
+    comment: string
+}
+
