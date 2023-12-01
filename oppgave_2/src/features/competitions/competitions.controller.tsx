dcff1741d22b5c1f989048b548d76e2e16f2ff72
Mathiadr <mathiadr@hiof.no>
2023-12-01 17:11:00 +0100

    Opprettet competition og lagret til databasen

--- oppgave_2/src/features/competitions/competitions.controller.tsx
+++ oppgave_2/src/features/competitions/competitions.controller.tsx
@@ -2,11 +2,15 @@ import { Competition, Result } from "@/types";
 import { NextRequest, NextResponse } from "next/server";
 import * as competitionService from './competitions.service'
 
-export const createCompetition = async (req: NextRequest): Promise<NextResponse<Result<Competition>>> => {
+export const createCompetition = async (req: NextRequest, id: string): Promise<NextResponse<Result<Competition>>> => {
     if(!req.body) return NextResponse.json(
         { success: false, error: "Request has no body"}, 
         { status: 400 }
     )
-    console.log(req.body)
-    return await competitionService.create(req)
+    if(id === "") return NextResponse.json(
+        { success: false, error: "athlete/[id] has no value"}, 
+        { status: 400 }
+    )
+
+    return await competitionService.create(req, id)
 }
