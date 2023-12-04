711e0a3f2226c5127ec43f544f5a75007073ece9
olikim <90384162+olikim@users.noreply.github.com>
2023-12-04 20:18:27 +0100

    Rydda i filer

--- oppgave_2/src/app/api/competitions/[athleteid]/[year]/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function GET(request: NextRequest){
-    return NextResponse.json({success: true}, {status: 200})
-}
-
-export function POST(request: NextRequest){
-    return NextResponse.json({success: true}, {status: 200})
-}
